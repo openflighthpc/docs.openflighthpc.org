08d606739673937e3483c5648fb5dc49e3916fea
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2023-08-21 16:23:23 +0100

    Hide/show tabs on window resize

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  if (!document.querySelector('.no-tabs')) {
-    document.querySelector('#tabs').style.display = 'flex';
-    document.querySelector('.search-container').style.display = 'inline';
-  }
+  renderTabs();
 });
+
+window.addEventListener('resize', () => {
+  renderTabs();
+});
+
+function renderTabs() {
+  if (
+    !document.querySelector('.no-tabs')
+    && !document.querySelector('.top-right-links .search-container').offsetParent
+  ) {
+    document.querySelector('#tabs').style.display = 'flex';
+  } else {
+    document.querySelector('#tabs').style.display = 'none';
+  }
+}
